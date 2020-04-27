5f220c17e43d2c001b6ae7effe4797ea3d5993de
anzamarska <anzamarska@gmail.com>
2020-04-27 13:28:41 +0200

    styled modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,15 +1,18 @@
 import React from "react";
 import styles from "./Modal.module.scss";
+import icon from "../../assets/bheart.png";
 
 const Modal = (props) => (
   <div className={styles.wrapper}>
     <button className={styles.button} onClick={props.closeModal}>
-      X
+      click to discover
     </button>
     <p className={styles.text}>
-      Welcome to the application. This application will allow you to choose a
-      category according which we will offere You the most beautiful wallpapers
-      on the Internet. Thank you for Your interests. Hava a nice search!
+      Welcome to the Wallpapers Search application. This application will allow
+      you to choose a category according which we will offere You the most
+      beautiful wallpapers on the Internet. Thank you for Your interests. Hava a
+      nice search!
+      <img className={styles.icon} src={icon} alt="" />
     </p>
   </div>
 );
