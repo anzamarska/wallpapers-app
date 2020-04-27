c53a05572e57f8361dca43a472b1d8a8ffbddf34
anzamarska <anzamarska@gmail.com>
2020-04-27 16:23:42 +0200

    add background img to modal, styled text on modal, add poligon property

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,17 +1,22 @@
 import React from "react";
 import styles from "./Modal.module.scss";
 import icon from "../../assets/bheart.png";
+import polygon from "../../assets/fog.jpg";
 
 const Modal = (props) => (
   <div className={styles.wrapper}>
     <button className={styles.button} onClick={props.closeModal}>
       click to discover
     </button>
+    <img className={styles.polygon} src={polygon} alt="" />
     <p className={styles.text}>
       Welcome to the Wallpapers Search application. This application will allow
       you to choose a category according which we will offere You the most
-      beautiful wallpapers on the Internet. Thank you for Your interests. Hava a
-      nice search!
+      beautiful wallpapers on the Internet. Thank you for Your interests.
+      <br />
+      Hava a nice search!
+      <br />
+      <br />
       <img className={styles.icon} src={icon} alt="" />
     </p>
   </div>
