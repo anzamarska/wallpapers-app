d1d57a189ce79f4a6679504025d1b4970ffcca07
anzamarska <anzamarska@gmail.com>
2020-05-01 21:08:57 +0200

    setting the position of photos and captions

--- src/components/PhotosGrid/Photo/Photo.js
+++ src/components/PhotosGrid/Photo/Photo.js
@@ -4,10 +4,18 @@ import style from "./Photo.module.scss";
 const Photo = (props) => {
   return (
     <>
-      <img className={style.photo} src={props.photo1} alt="" />
-      <p className={style.autorName}>{props.autor1} </p>
-      <img className={style.photo} src={props.photo2} alt="" />
-      <img className={style.photo} src={props.photo3} alt="" />
+      <div className={style.wrapper}>
+        <img className={style.photo} src={props.photo1} alt="" />
+        <p className={style.autorName}>{props.autor1} </p>
+      </div>
+      <div className={style.wrapper}>
+        <img className={style.photo} src={props.photo2} alt="" />
+        <p className={style.autorName}>{props.autor1} </p>
+      </div>
+      <div className={style.wrapper}>
+        <img className={style.photo} src={props.photo3} alt="" />
+        <p className={style.autorName}>{props.autor1} </p>
+      </div>
     </>
   );
 };
